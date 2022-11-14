a92367fc210634094459e8b4f91dc2ec5be8a2e9
Pito Peter <piotr.wozniak.2991@gmail.com>
2022-11-14 18:37:06 +0100

    choose card color

--- ManageDay/View/AddNewHabit.swift
+++ ManageDay/View/AddNewHabit.swift
@@ -12,7 +12,40 @@ struct AddNewHabit: View {
     @Environment(\.dismiss) var env
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack(spacing: 15) {
+                TextField("Title", text: $viewModel.title)
+                    .padding(.horizontal)
+                    .padding(.vertical, 10)
+                    .background(Color("TFBG").opacity(0.4), in: RoundedRectangle(cornerRadius: 6, style: .continuous))
+                
+                HStack(spacing: 0) {
+                    ForEach(1...7, id: \.self) { index in
+                        let color = "Card-\(index)"
+                        Circle()
+                            .fill(Color(color))
+                            .frame(width: 30, height: 30)
+                            .overlay(content: {
+                                if color == viewModel.habitColor {
+                                    Image(systemName: "checkmark")
+                                        .font(.caption.bold())
+                                        .foregroundColor(.white)
+                                }
+                            })
+                            .onTapGesture {
+                                withAnimation {
+                                    viewModel.habitColor = color
+                                }
+                            }
+                            .frame(maxWidth: .infinity)
+                    }
+                }
+                .padding(.top, 15)
+            }
+            Divider()
+                .padding(.vertical, 10)
+            
+        }
     }
 }
 
