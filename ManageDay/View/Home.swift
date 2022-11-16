0af0b733a8739e7762dae621e6fa1787050f0d97
Pito Peter <piotr.wozniak.2991@gmail.com>
2022-11-16 18:22:23 +0100

    card view

--- ManageDay/View/Home.swift
+++ ManageDay/View/Home.swift
@@ -30,7 +30,7 @@ struct Home: View {
             ScrollView(habits.isEmpty ? .init() : .vertical, showsIndicators: false) {
                 VStack(spacing: 15) {
                     ForEach(habits) { habit in
-                        
+                        habitCardView(habit: habit)
                     }
                     
                     Button {
@@ -58,6 +58,85 @@ struct Home: View {
             AddNewHabit()
                 .environmentObject(viewModel)
         }
+    }
+    func habitCardView(habit: Habit) -> some View {
+        VStack(spacing: 6) {
+            HStack {
+                Text(habit.title ?? "")
+                    .font(.callout)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                
+                Image(systemName: "bell.badge.fill")
+                    .font(.callout)
+                    .foregroundColor(Color(habit.color ?? "Card-1"))
+                    .scaleEffect(0.9)
+                    .opacity(habit.isRemainderOn ? 1 : 0)
+                
+                Spacer()
+                
+                let count = (habit.weekDays?.count ?? 0)
+                    Text(count == 7
+                         ? "Everyday"
+                         : count == 1 ? "Once a week" : "\(count) times a week")
+                    .font(.caption)
+                    .foregroundColor(.gray)
+            }
+            .padding(.horizontal, 10)
+            
+            let calendar = Calendar.current
+            let currentWeek = calendar.dateInterval(of: .weekOfMonth, for: Date())
+            let symbols = calendar.weekdaySymbols
+            let startDate = currentWeek?.start ?? Date()
+            let activeWeekDays = habit.weekDays ?? []
+            let activePlot = symbols.indices.compactMap { index -> (String, Date) in
+                let currentDate = calendar.date(byAdding: .day, value: index, to: startDate)
+                return (symbols[index], currentDate!)
+            }
+            
+            HStack(spacing: 0) {
+                ForEach(activePlot.indices, id: \.self) { index in
+                    let item = activePlot[index]
+                    
+                    VStack(spacing: 6) {
+                        Text(item.0.prefix(3))
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                        let status = activeWeekDays.contains { day in
+                            return day == item.0
+                        }
+                        Text(getData(date: item.1))
+                            .font(.system(size: 32))
+                            .fontWeight(.semibold)
+                            .padding(8)
+                            .foregroundColor(status ? .white : .primary)
+                            .background {
+                                Circle()
+                                    .fill(Color(habit.color ?? "Color-1"))
+                            }
+                    }
+                    .frame(maxWidth: .infinity)
+                }
+            }
+            .padding()
+        }
+        .padding(.vertical)
+        .padding(.horizontal, 6)
+        .background {
+            RoundedRectangle(cornerRadius: 12, style: .continuous)
+                .fill(Color("TFBG").opacity(0.5))
+        }
+        .onTapGesture {
+            viewModel.editHabit = habit
+            viewModel.restoreEditData()
+            viewModel.addNewHabit.toggle()
+        }
+    }
+    func getData(date: Date) -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd"
+        return formatter.string(from: date)
     }
 }
 
